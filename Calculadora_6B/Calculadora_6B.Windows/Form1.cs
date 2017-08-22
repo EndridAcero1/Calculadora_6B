54f3a04f5b11f1cf85af9abc659555930f67ec22
Endrid Acero Velazquez <acero_pumas95@hotmail.com>
2017-08-22 18:09:22 -0500

    Calculadora_6B

---
+++ Calculadora_6B/Calculadora_6B.Windows/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculadora_6B.Libreria;
+
+namespace Calculadora_6B.Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           Metodos misMetodos = new Libreria.Metodos();
+            MessageBox.Show(misMetodos.Saludar());
+        }
+    }
+}
